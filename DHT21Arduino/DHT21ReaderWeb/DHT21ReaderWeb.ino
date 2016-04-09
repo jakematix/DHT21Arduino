9a1926814fd360456afc64bd9ef535f12f5566ca
Jarmo Kant <jarmo.kant@gmail.com>
2016-04-09 15:09:50 +0300

    small changes

--- DHT21Arduino/DHT21ReaderWeb/DHT21ReaderWeb.ino
+++ DHT21Arduino/DHT21ReaderWeb/DHT21ReaderWeb.ino
@@ -13,7 +13,7 @@
 
 // Connect pin 1 (on the left) of the sensor to +5V
 // NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
+// to 3.3V instead of 5V! Yun is operating at 5V!
 // Connect pin 2 of the sensor to whatever your DHTPIN is
 // Connect pin 4 (on the right) of the sensor to GROUND
 // Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
